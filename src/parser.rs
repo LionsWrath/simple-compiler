5a91a3c52a0be220fb192fdfa90986373df7cf51
Caio Henrique Segawa Tonetti <caio.tonetti@gmail.com>
2024-03-13 23:05:26 -0300

    the beginning

--- src/parser.rs
+++ src/parser.rs
@@ -1,1 +1,13 @@
 // This is where the main parser code will be.
+
+struct Lexer;
+
+impl Lexer {
+    pub fn new(source: String) -> Self {}
+    pub fn nextChar(&self) {}
+    pub fn peek(&self) {}
+    pub fn abort(&self, message: String) {}
+    pub fn skipWhitespace(&self) {}
+    pub fn skipComment(&self) {}
+    pub fn getToken(&self) {}
+}
