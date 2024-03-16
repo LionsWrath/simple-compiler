b9f205d3ad04df14a914a35329be9a20629655aa
Caio Henrique Segawa Tonetti <caio.tonetti@gmail.com>
2024-03-16 00:34:42 -0300

    noice

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::fs::File;
+use std::io::Read;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let mut f = File::open("file.txt")?;
+    let mut source = vec![];
+
+    f.read_to_end(&mut source);
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,15 +1,42 @@
-// This is where the main parser code will be.
-
 struct Lexer {
-    source: String
+    source: String,
+    curChar: char,
+    curPos: Option<usize>,
 };
 
 impl Lexer {
-    pub fn new(source: String) -> Self {}
-    pub fn nextChar(&self) {}
+
+    pub fn new(source: &Vec<char>) -> Self {
+        let lexer = Lexer {
+           source: Vec<char>,
+           curChar: '',
+           curPos: None,
+        };
+
+        lexer.nextChar();
+        lexer
+    }
+
+    pub fn nextChar(&mut self) {
+        match self.curPos {
+            Some(x) =>  {
+                self.curPos = Some(x + 1);
+                if x + 1 >= self.source.len() {
+                    self.curChar = '\0';
+                } else {
+                    self.curChar = self.source[self.curPos];
+                }
+            },
+            None => {
+                self.curPos = Some(0);
+                self.curChar = self.source[0];
+            }
+        }
+    }
+
     pub fn peek(&self) {}
     pub fn abort(&self, message: String) {}
     pub fn skipWhitespace(&self) {}
     pub fn skipComment(&self) {}
-        pub fn getToken(&self) {}
+    pub fn getToken(&self) {}
 }
