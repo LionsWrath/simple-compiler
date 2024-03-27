977b8c7f49ead0a60bd1f12055bd0b2bc2c4886e
Caio Henrique Segawa Tonetti <caio.tonetti-c@newsela.com>
2024-03-26 23:14:14 -0300

    tokenization first part

--- src/lex.rs
+++ src/lex.rs
@@ -1,3 +1,94 @@
+use std::fmt;
+
+pub enum TokenType {
+	EOF = -1,
+	NEWLINE = 0,
+	NUMBER = 1,
+	IDENT = 2,
+	STRING = 3,
+	// Keywords
+	LABEL = 101,
+	GOTO = 102,
+	PRINT = 103,
+	INPUT = 104,
+	LET = 105,
+	IF = 106,
+	THEN = 107,
+	ENDIF = 108,
+	WHILE = 109,
+	REPEAT = 110,
+	ENDWHILE = 111,
+	// Operators
+	EQ = 201,
+	PLUS = 202,
+	MINUS = 203,
+	ASTERISK = 204,
+	SLASH = 205,
+	EQEQ = 206,
+	NOTEQ = 207,
+	LT = 208,
+	LTEQ = 209,
+	GT = 210,
+	GTEQ = 211,
+}
+
+pub struct Token {
+    pub text: Vec<char>,
+    pub kind: TokenType
+}
+
+impl Token {
+    pub fn new(text: Vec<char>, kind: TokenType) -> Self {
+        Token {
+            text,
+            kind
+        }
+    }
+}
+
+impl fmt::Debug for Token {
+    fn  fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_tuple("")
+            .field(&self.text)
+            .field(&self.kind)
+            .finish()
+    }
+}
+
+impl fmt::Debug for TokenType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            TokenType::EOF => write!(f, "eof"),
+            TokenType::NEWLINE => write!(f, "newline"),
+            TokenType::NUMBER => write!(f, "number"),
+            TokenType::IDENT => write!(f, "ident"),
+            TokenType::STRING => write!(f, "string"),
+            TokenType::LABEL => write!(f, "label"),
+            TokenType::GOTO => write!(f, "goto"),
+            TokenType::PRINT => write!(f, "print"),
+            TokenType::INPUT => write!(f, "input"),
+            TokenType::LET => write!(f, "let"),
+            TokenType::IF => write!(f, "if"),
+            TokenType::THEN => write!(f, "then"),
+            TokenType::ENDIF => write!(f, "endif"),
+            TokenType::WHILE => write!(f, "while"),
+            TokenType::REPEAT => write!(f, "repeat"),
+            TokenType::ENDWHILE => write!(f, "endwhile"),
+            TokenType::EQ => write!(f, "eq"),
+            TokenType::PLUS => write!(f, "plus"),
+            TokenType::MINUS => write!(f, "minus"),
+            TokenType::ASTERISK => write!(f, "asterisk"),
+            TokenType::SLASH => write!(f, "slash"),
+            TokenType::EQEQ => write!(f, "eqeq"),
+            TokenType::NOTEQ => write!(f, "noteq"),
+            TokenType::LT => write!(f, "lt"),
+            TokenType::LTEQ => write!(f, "lteq"),
+            TokenType::GT => write!(f, "gt"),
+            TokenType::GTEQ => write!(f, "gteq"),
+        }
+    }
+}
+
 pub struct Lexer {
     pub source: Vec<char>,
     pub cur_char: char,
@@ -8,7 +99,7 @@ impl Lexer {
 
     pub fn new(source: Vec<char>) -> Self {
         let mut lexer = Lexer {
-           source: source,
+           source,
            cur_char: '\0',
            cur_pos: None,
         };
@@ -20,11 +111,11 @@ impl Lexer {
     pub fn next_char(&mut self) {
         match self.cur_pos {
             Some(x) =>  {
-                self.cur_pos = Some(x + 1);
                 if x + 1 >= self.source.len() {
                     self.cur_char = '\0';
                 } else {
                     self.cur_char = self.source[x + 1];
+                    self.cur_pos = Some(x + 1);
                 }
             },
             None => {
@@ -46,8 +137,82 @@ impl Lexer {
         }
     }
 
-    pub fn abort(&self, message: String) {}
-    pub fn skip_whitespace(&self) {}
-    pub fn skip_comment(&self) {}
-    pub fn get_token(&self) {}
+    pub fn abort(&self, message: String) {
+        panic!("[LEXER ERROR] {message}");
+    }
+
+    pub fn skip_whitespace(&mut self) {
+        while self.cur_char == ' ' || self.cur_char == '\t' || self.cur_char == '\r' {
+            self.next_char();
+        }
+    }
+
+    pub fn skip_comment(&mut self) {
+        if self.cur_char == '#' {
+            while self.cur_char != '\n' {
+                self.next_char();
+            }
+        }
+    }
+    
+    pub fn get_token(&mut self) -> Token {
+
+        let token: Token;
+
+        self.skip_whitespace();
+        self.skip_comment();
+
+        token = match self.cur_char {
+            '+' => Token::new(vec![self.cur_char], TokenType::PLUS),
+            '-' => Token::new(vec![self.cur_char], TokenType::MINUS),
+            '*' => Token::new(vec![self.cur_char], TokenType::ASTERISK),
+            '/' => Token::new(vec![self.cur_char], TokenType::SLASH),
+            '>' => {
+                if self.peek() == '=' {
+                    let last_char = self.cur_char;
+                    self.next_char();
+
+                    Token::new(vec![last_char, self.cur_char], TokenType::GTEQ)
+                } else {
+                    Token::new(vec![self.cur_char], TokenType::GT)
+                }
+            },
+            '<' => {
+                if self.peek() == '=' {
+                    let last_char = self.cur_char;
+                    self.next_char();
+
+                    Token::new(vec![last_char, self.cur_char], TokenType::LTEQ)
+                } else {
+                    Token::new(vec![self.cur_char], TokenType::LT)
+                }
+            },
+            '!' => {
+                if self.peek() == '=' {
+                    let last_char = self.cur_char;
+                    self.next_char();
+
+                    Token::new(vec![last_char, self.cur_char], TokenType::NOTEQ)
+                } else {
+                    panic!("[LEXER] ERROR: Expected !=, got !{}", self.peek())
+                }
+            },
+            '=' => {
+                if self.peek() == '=' {
+                    let last_char = self.cur_char;
+                    self.next_char();
+
+                    Token::new(vec![last_char, self.cur_char], TokenType::EQEQ)
+                } else {
+                    Token::new(vec![self.cur_char], TokenType::EQ)
+                }
+            },
+            '\n' => Token::new(vec![self.cur_char], TokenType::NEWLINE),
+            '\0' => Token::new(vec![], TokenType::EOF),
+            other => panic!("[LEXER] ERROR: Unknown token {other}")
+        };
+
+        self.next_char();
+        token
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,17 @@ use std::io::Read;
 use failure::Error;
 
 fn main() -> Result<(), Error>{
-    let mut f = File::open("src/examples/file.txt")?;
+    let mut f = File::open("src/examples/file2.txt")?;
     let mut raw: Vec<u8> = Vec::new();
     let _ = f.read_to_end(&mut raw);
     let source: Vec<char> = raw.iter().map(|b| *b as char).collect::<Vec<_>>();
 
     let mut lexer = lex::Lexer::new(source);
+    let mut token = lexer.get_token();
 
-    while lexer.peek() != '\0' {
-        println!("{}", lexer.cur_char);
-        lexer.next_char();
+    while !matches!(token.kind, lex::TokenType::EOF) {
+        println!("{:?} POS: {:?}", token, lexer.cur_pos);
+        token = lexer.get_token();
     }
 
     return Ok(());
