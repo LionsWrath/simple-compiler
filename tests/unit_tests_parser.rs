fc882a05ea7030ef5dc8f502efb52adbaa88819d
Caio Henrique Segawa Tonetti <caio.tonetti@gmail.com>
2024-04-05 23:13:38 -0300

    adding some tests

---
+++ tests/unit_tests_parser.rs
@@ -0,0 +1,15 @@
+#[path = "../src/lex.rs"] mod lex;
+#[path = "../src/parse.rs"] mod parse;
+#[path = "../src/utils/utils.rs"] mod utils;
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_placeholder() {
+        assert!(True));
+    }
+
+}
